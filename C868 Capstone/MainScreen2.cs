74aa2dd9356f229da693eb89a0c6a7f462050e5b
gosupark <hpark41@wgu.edu>
2020-08-07 16:02:06 -0700

    Add project files.

---
+++ C868 Capstone/MainScreen2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C868_Capstone
+{
+	public partial class MainScreen2 : Form
+	{
+		public MainScreen2()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ C868 Capstone/MainScreen3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C868_Capstone
+{
+	public partial class MainScreen3 : Form
+	{
+		public MainScreen3()
+		{
+			InitializeComponent();
+		}
+	}
+}
